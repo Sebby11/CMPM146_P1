9170755817a2babef2dea3bb0f15989df12541c9
Sebby11 <used4sebastianid@gmail.com>
2020-10-11 13:03:52 -0700

    added dijkstra's shortest path

--- src/p1.py
+++ src/p1.py
@@ -1,8 +1,26 @@
+#MINE
 from p1_support import load_level, show_level, save_level_costs
 from math import inf, sqrt
 from heapq import heappop, heappush
 
+#src = coordinates of waypoint
+#destination = coordinates of destination
+#graph = level
+#adj = passes function, navigation_edges
+"""
+STEPS OF DIJKSTRA'S (explores lowest paths first)
 
+1. start at source cell
+2. create priority queue of nodes connected
+3. from the source look at the shortest path in the queue(heappop)
+4. Go to node w/ shortest path & take note of where you came from
+	coupling that value w/ the new current node
+5. Look at the new current node's neighbors & add them to the queue
+	If any of the neighbors have a shorter path than previous replace
+	the path value on the queue w/ the new distance & where it came from
+6. after all nodes have been looked at remove from queue & move to
+	next shortest
+"""
 def dijkstras_shortest_path(initial_position, destination, graph, adj):
     """ Searches for a minimal cost path through a graph using Dijkstra's algorithm.
 
@@ -17,6 +35,7 @@ def dijkstras_shortest_path(initial_position, destination, graph, adj):
         Otherwise, return None.
 
     """
+    #create heap priority queue representing frontier
     priQ = [(0, initial_position)]
 
     #came from dictionary
@@ -25,33 +44,36 @@ def dijkstras_shortest_path(initial_position, destination, graph, adj):
     #variable to keep track of cost cost so far
     costs = {initial_position:0}
 
+    minimum	 = 0.0
+
     #main dijkstra's loop
     while len(priQ) != 0:
-        #curr = (col, row)
-        curr = heappop(priQ)
+    	#curr = (col, row)
+    	curr = heappop(priQ)
 
-        if curr[1] == destination:
-            #get list of values 
+    	if curr[1] == destination:
+            #get list of values
             tmp = curr[1]
             li = [tmp]
+            minimum = costs[tmp]
             #shorten by enumeration?
+            minimum = min(costs[tmp], minimum)
             while cellFrom[tmp] != None:
-                tmp = cellFrom[tmp]
-                li.append(tmp)
+            	tmp = cellFrom[tmp]
+            	li.append(tmp)
+            print(minimum)
             return li
 
-        #i = ((col, row), cost)
-        for i in adj(graph, curr[1]):
+    	#i = ((col, row), cost)
+    	for i in adj(graph, curr[1]):
             new_cost = costs[curr[1]] + i[1]
             if i[0] not in costs or new_cost < costs[i[0]]:
-                costs[i[0]] = new_cost
-                heappush(priQ, i[::-1])    #reverse so queue detects distance first (position, distance)
-                print(priQ)
-                cellFrom[i[0]] = curr[1]
-            
-        
+            	costs[i[0]] = new_cost
+            	priority = new_cost
+            	tmpy = (priority, i[0])
+            	heappush(priQ, tmpy)		#reverse so queue detects distance first (position, distance)
+            	cellFrom[i[0]] = curr[1]
     return None
-
 
 def dijkstras_shortest_path_to_all(initial_position, graph, adj):
     """ Calculates the minimum cost to every reachable cell in a graph from the initial_position.
@@ -68,14 +90,17 @@ def dijkstras_shortest_path_to_all(initial_position, graph, adj):
 
 
 def navigation_edges(level, cell):
-    """ Provides a list of adjacent cells and their respective costs from the given cell.
+    """ Provides a list of adjacent cells and their 
+    	respective costs from the given cell.
 
     Args:
-        level: A loaded level, containing walls, spaces, and waypoints.
+        level: A loaded level, containing walls, spaces, 
+                and waypoints.
         cell: A target location.
 
     Returns:
-        A list of tuples containing an adjacent cell's coordinates and the cost of the edge joining it and the
+        A list of tuples containing an adjacent cell's 
+        coordinates and the cost of the edge joining it and the
         originating cell.
 
         E.g. from (0,0):
@@ -84,41 +109,63 @@ def navigation_edges(level, cell):
              ((1,1), 1.4142135623730951),
              ... ]
     """
+    
     toCheck = [
-        (cell[0]-1, cell[1]-1), #top left
-        (cell[0], cell[1]-1),   #top
-        (cell[0]+1, cell[1]-1), #top right
-        (cell[0]-1, cell[1]),   #left
-        (cell[0]+1, cell[1]),   #right
-        (cell[0]-1, cell[1]+1), #bottom left
-        (cell[0], cell[1]+1),   #bottom
-        (cell[0]+1, cell[1]+1)  #bottom right
+    	(cell[0]-1, cell[1]-1), #top left
+    	(cell[0], cell[1]-1),	#top
+    	(cell[0]+1, cell[1]-1),	#top right
+        (cell[0]-1, cell[1]), 	#left
+        (cell[0]+1, cell[1]),	#right
+        (cell[0]-1, cell[1]+1),	#bottom left
+        (cell[0], cell[1]+1),	#bottom
+        (cell[0]+1, cell[1]+1)	#bottom right
     ]
     diags = [
-        (cell[0]-1, cell[1]-1), #top left
-        (cell[0]+1, cell[1]-1), #top right
-        (cell[0]-1, cell[1]+1), #bottom left
-        (cell[0]+1, cell[1]+1)  #bottom right
+    	(cell[0]-1, cell[1]-1), #top left
+    	(cell[0]+1, cell[1]-1),	#top right
+    	(cell[0]-1, cell[1]+1),	#bottom left
+    	(cell[0]+1, cell[1]+1)	#bottom right
     ]
     diagFlag = False
     nextSpaces = []
 
     for i in toCheck:
-        if i in diags:
+    	if i in diags:
             diagFlag = True
 
-        #check 8 cells around
-        if i in level['spaces']:
+    	#check 8 cells around
+    	if i in level['spaces']:
             if not(diagFlag):
-                cost = (0.5 * (level['spaces'][cell])) + (0.5 * (level['spaces'][i]))
+            	cost = (0.5 * (level['spaces'][cell])) + (0.5 * (level['spaces'][i]))
             else:
-                cost = ((0.5 * sqrt(2)) * level['spaces'][cell]) + ((0.5 * sqrt(2)) * level['spaces'][i])
+            	cost = ((0.5 * sqrt(2)) * level['spaces'][cell]) + ((0.5 * sqrt(2)) * level['spaces'][i])
             
             nextSpaces.append((i, cost))
-        diagFlag = False
+    	diagFlag = False
 
 
     return nextSpaces
+
+    """
+    listy = []
+    listOfHorizCells = [(cell[0], cell[1]-1), (cell[0]-1, cell[1]), (cell[0]+1, cell[1]), (cell[0], cell[1]+1)]
+    ListOfDiagCells = [(cell[0]-1, cell[1]-1), (cell[0]+1, cell[1]-1), (cell[0]-1, cell[1]+1), (cell[0]+1, cell[1]+1)]
+    DictOfSpaces = level["spaces"]
+
+    HorizMovableKeys = list(listOfHorizCells & DictOfSpaces.keys())
+    DiagMovableKeys = list(ListOfDiagCells & DictOfSpaces.keys())
+
+    HorizDict = {x:(0.5*DictOfSpaces.get(cell) + 0.5*DictOfSpaces.get(x)) for x in HorizMovableKeys}
+    DiagDict = {x:(0.5*sqrt(2)*DictOfSpaces.get(cell) + 0.5*sqrt(2)*DictOfSpaces.get(x)) for x in DiagMovableKeys}
+
+    FinalDict = {**HorizDict, **DiagDict}
+
+    for i in FinalDict:
+        #print('key:' + str(i) + 'value:' + str(FinalDict[i]))
+        listy.append(tuple((i, FinalDict[i])))
+
+    return listy
+    """
 
 
 def test_route(filename, src_waypoint, dst_waypoint):
@@ -171,21 +218,33 @@ def cost_to_all_cells(filename, src_waypoint, output_filename):
 
 
 if __name__ == '__main__':
-    filename, src_waypoint, dst_waypoint = 'example.txt', 'a','e'
+    filename, src_waypoint, dst_waypoint = 'example.txt', 'a','c'
 
     #TESTING navigation_edges
     print("********************* navigation_edges TEST *************************")
     level = load_level(filename)
-    show_level(level)
+    #show_level(level)
     #print("Walls: \n", level['waypoints'],
-    #   "\n Spaces:\n ", level['spaces'],
-    #   "\n waypoints: \n", level['waypoints'])
-    neibs = navigation_edges(level, (3,10))
-    print("NEIGHBORS OF (2,1): \n" , neibs)
+    #	"\n Spaces:\n ", level['spaces'],
+    #	"\n waypoints: \n", level['waypoints'])
+    #neibs = navigation_edges(level, (1,1))
+    #print("NEIGHBORS OF (1,1): \n" , neibs)
 
     #TESTING dijkstras_shortest_path (a -> e)
-    print("\n********************* djik_short_path/test_route TEST *************************")
-    test_route(filename, "b", "e")
+    print("********************* djik_short_path/test_route TEST *************************")
+    
+    print("A TO B")
+    test_route(filename, "a", "b")
+    print("A TO C")
+    test_route(filename, "a", "c")
+    print("A TO D")
+    test_route(filename, "a", "d")
+    
+    print("A TO E")
+    test_route(filename, "a", "e")
+
+    #dijkstras_shortest_path(level['waypoints']['a'], level['waypoints']['e'], level, navigation_edges)
+
 
     # Use this function call to find the route between two waypoints.
     #test_route(filename, src_waypoint, dst_waypoint)
